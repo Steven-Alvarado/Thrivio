49e5a9711cc79efd0a8727e3964fdf65aab0f81a
Steven-Alvarado <Steven67alvarado@gmail.com>
2024-07-18 13:56:52 -0400

    implementing api.ts

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,1 +1,45 @@
 /*api calls and external services*/
+import axios from 'axios';
+
+const API_URL = 'http://ip-address:3000/api'
+
+const api = axios.create({
+    baseURL: API_URL,
+    headers: {
+        'Content-Type': 'application/json',
+    },
+});
+
+export const setAuthToken = (token) => {
+    if(token) {
+        api.defaults.headers.common['x-auth-token'] = token;
+    }
+    else {
+        delete api.defaults.headers.common['x-auth-token'];
+    }
+};
+
+export default api;
+
+// Service functions 
+
+export const login = async (email, password) => {
+    try {
+      const response = await api.post('/auth/login', { email, password });
+      return response.data;
+    } catch (error) {
+      throw error.response.data;
+    }
+  };
+  
+  export const getProfile = async () => {
+    try {
+      const response = await api.get('/users/profile');
+      return response.data;
+    } catch (error) {
+      throw error.response.data;
+    }
+  };
+  
+  // TODO: Add more functions for other API endpoints
+    
